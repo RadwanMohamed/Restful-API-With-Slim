4fce9da93a10a9f9667f479442163764814a4e05
RadwanMohamed <radwanmohamed334@gmail.com>
2018-02-18 12:41:34 +0200

    send simple json

--- public/index.php
+++ public/index.php
@@ -19,4 +19,16 @@ $app->post('/API/first',function (Request $request,Response $response){
    $var['phone'] = filter_var($data['phone'],FILTER_SANITIZE_STRING);
    $response->getBody()->write("welcome ".$var['name']."\n"."your phone ".$var['phone']);
 });
+// send json data
+$app->get("/API/sendsimplejson/{firstname}/{lastname}",function(Request $Request,Response $Response,$args){
+    $firstname     = $args['firstname'];
+    $lastname = $args['lastname'];
+    $SendData = [];
+    $SendData['status']=200;
+    $SendData['messsage']="send simple json";
+    $SendData['data']=[];
+    $SendData['data']['firstname']=$firstname;
+    $SendData['data']['lastname'] =$lastname;
+    $Response->getBody()->write(json_encode($SendData));
+});
 $app->run();
